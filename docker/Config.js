dafd9f5027b07161346ce75461113cbd276d511a
AndiMajore <andi.majore@googlemail.com>
2022-02-14 17:15:00 +0100

    adjusted for dock1

--- docker/Config.js
+++ docker/Config.js
@@ -1,7 +1,7 @@
-const HOST = "ml-s-zbhdock3.ad.uni-hamburg.de"
-export const CONTEXT_PATH="/"
+const HOST = "ml-s-zbhdock1.ad.uni-hamburg.de"
+export const CONTEXT_PATH="/digest_backend"
 export const BACKEND_PORT= "8090"
 export const HOST_URL = "http://" + HOST + ":" + BACKEND_PORT
 //export const SOCKET_URL = "http://" + HOST + ":8090"
-export const PATH_PREFIX= ""
-export const STATIC_PATH="/static/"
+export const PATH_PREFIX= "/digest"
+export const STATIC_PATH="/digest/static/"
