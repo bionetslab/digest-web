100e941e6d7a7c370eedf47dfceba020e7021395
AndiMajore <andi.majore@googlemail.com>
2022-02-14 22:10:59 +0100

    adjusted backend port

--- docker/Config.js
+++ docker/Config.js
@@ -1,6 +1,6 @@
 const HOST = "ml-s-zbhdock1.ad.uni-hamburg.de"
 export const CONTEXT_PATH="/digest_backend"
-export const BACKEND_PORT= "80"
+export const BACKEND_PORT= "8080"
 export const HOST_URL = "http://" + HOST + ":" + BACKEND_PORT
 //export const SOCKET_URL = "http://" + HOST + ":8090"
 export const PATH_PREFIX= "/digest/"
