93642b608dda76d33ef06f2c81cdb67ea7e77c95
Klaudia <klaudia.adamowicz.91@gmail.com>
2022-02-23 19:59:33 +0100

    added selection texts

--- src/components/validation/Selection.vue
+++ src/components/validation/Selection.vue
@@ -2,7 +2,7 @@
   <div style="width: 100%; display: flex; justify-content: center">
     <v-card style="width: 45%; height: 300px; margin:8px">
       <v-card-title>Set</v-card-title>
-      <v-card-subtitle style="height: 52px">Compare similarity inside the set.</v-card-subtitle>
+      <v-card-subtitle style="height: 52px">Validate gene or disease sets w.r.t. functional or genetic coherence.</v-card-subtitle>
       <div style="width: 100%; display: flex; align-self: flex-end; margin-top: auto; margin-bottom: auto;">
         <v-card style="width: 50%; height: 200px;" :elevation="hover_set_gene ? 10:0" :flat="!hover_set_gene"
                 @mouseenter="hover_set_gene=true" @mouseleave="hover_set_gene=false">
@@ -12,8 +12,7 @@
               src="https://media.istockphoto.com/photos/blue-chromosome-dna-and-gradually-glowing-flicker-light-matter-when-picture-id1297146235?b=1&k=20&m=1297146235&s=170667a&w=0&h=vUyHfisjnfzyhzeZvYzt23iaHSEl6UkpoiFctK4BMbc="
           >
             <v-card-title>Gene</v-card-title>
-            <v-card-subtitle v-show="hover_set_gene">asdfj saöldfj aösldkfj aösdklfa södklf asödlfkja södlfkjas
-              ödflkjsd öflksajd föskldj f
+            <v-card-subtitle v-show="hover_set_gene">Validate gene sets w.r.t. functional coherence.
             </v-card-subtitle>
             <v-card-actions v-show="hover_set_gene">
               <div style="width: 100%; display: flex; justify-content: center">
@@ -33,8 +32,7 @@
               src="https://images.medicinenet.com/images/article/main_image/what-is-conversion-disorder.jpg"
           >
             <v-card-title>Disease</v-card-title>
-            <v-card-subtitle v-show="hover_set_disorder">asdfj saöldfj aösldkfj aösdklfa södklf asödlfkja
-              södlfkjas ödflkjsd öflksajd föskldj f
+            <v-card-subtitle v-show="hover_set_disorder">Validate disease sets w.r.t. genetic coherence.
             </v-card-subtitle>
             <v-card-actions v-show="hover_set_disorder">
               <div style="width: 100%; display: flex; justify-content: center">
@@ -48,9 +46,9 @@
       </div>
     </v-card>
     <v-card style="width: 45%; height: 300px; margin:8px">
-      <v-card-title>Cluster
+      <v-card-title>Clustering
       </v-card-title>
-      <v-card-subtitle style="height: 52px">Compare inter and intra cluster similarities.</v-card-subtitle>
+      <v-card-subtitle style="height: 52px">Validate gene or disease clusterings w.r.t. functional or genetic coherence.</v-card-subtitle>
       <div style="width: 100%; display: flex; align-self: flex-end; margin-top: auto; margin-bottom: auto;">
         <v-card style="width: 50%; height: 200px;" :elevation="hover_cluster_gene ? 10:0"
                 :flat="!hover_cluster_gene"
@@ -61,9 +59,7 @@
               src="https://media.istockphoto.com/photos/blue-chromosome-dna-and-gradually-glowing-flicker-light-matter-when-picture-id1297146235?b=1&k=20&m=1297146235&s=170667a&w=0&h=vUyHfisjnfzyhzeZvYzt23iaHSEl6UkpoiFctK4BMbc="
           >
             <v-card-title>Gene</v-card-title>
-            <v-card-subtitle v-show="hover_cluster_gene">asdfj saöldfj aösldkfj aösdklfa södklf asödlfkja
-              södlfkjas
-              ödflkjsd öflksajd föskldj f
+            <v-card-subtitle v-show="hover_cluster_gene">Validate gene clustering w.r.t. functional coherence.
             </v-card-subtitle>
             <v-card-actions v-show="hover_cluster_gene">
               <div style="width: 100%; display: flex; justify-content: center">
@@ -83,8 +79,7 @@
               src="https://images.medicinenet.com/images/article/main_image/what-is-conversion-disorder.jpg"
           >
             <v-card-title>Disease</v-card-title>
-            <v-card-subtitle v-show="hover_cluster_disorder">asdfj saöldfj aösldkfj aösdklfa södklf asödlfkja
-              södlfkjas ödflkjsd öflksajd föskldj f
+            <v-card-subtitle v-show="hover_cluster_disorder">Validate disease clustering w.r.t. genetic coherence.
             </v-card-subtitle>
             <v-card-actions v-show="hover_cluster_disorder">
               <div style="width: 100%; display: flex; justify-content: center">
