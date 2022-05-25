b0dcb12bd1e8c6081f9eeabaf126c881a090bd40
AndiMajore <andi.majore@googlemail.com>
2022-05-25 19:07:17 +0200

    added optimization to build stage

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,38 @@
+const TerserPlugin = require('terser-webpack-plugin')
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all') // required for purgecss
+const path = require('path') // re quired for purgecss
 module.exports = {
     transpileDependencies: [
         'vuetify'
     ],
+    configureWebpack: {
+        module: {
+            exprContextCritical: false
+        },
+        optimization: {
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        warnings: false,
+                        compress: {
+                            drop_console: true
+                        }
+                    }
+                }),
+            ],
+            splitChunks: {
+                chunks: 'all'
+            }
+        },
+        plugins: [new PurgecssPlugin({
+            paths: glob.sync([
+                path.join(__dirname, './public/index.html'),
+                path.join(__dirname, './src/**/*.vue'),
+                path.join(__dirname, './src/**/*.html')
+            ])
+        })]
+    },
     pluginOptions: {
         compression: {
             gzip: {
