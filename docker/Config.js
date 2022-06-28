d7393f208c8d8842b824c482aae233ada19b17b0
AndiMajore <andi.majore@googlemail.com>
2022-06-28 10:51:41 +0200

    preparation for arenal

--- docker/Config.js
+++ docker/Config.js
@@ -1,7 +1,8 @@
-const HOST = "api.digest-validation.net"
+// const HOST = "api.digest-validation.net"
+const HOST ="digest-backend.zbh.uni-hamburg.de"
 export const CONTEXT_PATH="/"
 // export const BACKEND_PORT= "8080"
-export const HOST_URL = "https://" + HOST //+ ":" + BACKEND_PORT
-//export const SOCKET_URL = "http://" + HOST + ":8090"
+// export const HOST_URL = "https://" + HOST //+ ":" + BACKEND_PORT
+export const HOST_URL = "http://" + HOST //+ ":" + BACKEND_PORT
 export const PATH_PREFIX= "/"
 export const STATIC_PATH="/static/"
