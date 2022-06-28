2ccfc901e3e97005cdc6bbc4071a16418d50c29f
AndiMajore <andi.majore@googlemail.com>
2022-06-28 15:58:04 +0200

    adjusted digest config for ssl

--- docker/Config.js
+++ docker/Config.js
@@ -1,8 +1,6 @@
-// const HOST = "api.digest-validation.net"
-const HOST ="digest-backend.zbh.uni-hamburg.de"
+const HOST = "api.digest-validation.net"
 export const CONTEXT_PATH="/"
 // export const BACKEND_PORT= "8080"
-// export const HOST_URL = "https://" + HOST //+ ":" + BACKEND_PORT
-export const HOST_URL = "http://" + HOST //+ ":" + BACKEND_PORT
+export const HOST_URL = "https://" + HOST //+ ":" + BACKEND_PORT
 export const PATH_PREFIX= "/"
 export const STATIC_PATH="/static/"
