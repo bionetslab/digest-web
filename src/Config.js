92f2f39706086534c10157572d7a5971dd75c143
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2026-03-30 10:56:32 +0200

    added dev_server start target to test with deployed backend

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,7 @@
-const HOST = "localhost"
+const HOST = import.meta.env.VITE_BACKEND_HOST || "localhost"
 export const CONTEXT_PATH="/"
-export const BACKEND_PORT= "8001"
-export const HOST_URL = "http://" + HOST + ":" + BACKEND_PORT
+export const BACKEND_PORT= import.meta.env.VITE_BACKEND_PORT || "8001"
+export const HOST_URL = import.meta.env.VITE_BACKEND_URL || ("http://" + HOST + ":" + BACKEND_PORT)
 //export const SOCKET_URL = "http://" + HOST + ":8090"
 export const PATH_PREFIX= ""
 export const STATIC_PATH="/static/"
